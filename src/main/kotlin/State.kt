71d943fef4028c8cbfa74e13ab3f08314bba21fc
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2018-07-20 10:48:49 -0700

    Add tracing

--- src/main/kotlin/State.kt
+++ src/main/kotlin/State.kt
@@ -1,22 +1,61 @@
 package lambdallama.github.io
 
 class State {
+    private val traceListeners: ArrayList<TraceListener> = ArrayList()
+
     fun loadFrom(/* */) { }
 
+    fun addTraceListener(traceListener: TraceListener) {
+        traceListeners.add(traceListener)
+    }
+
     /* Commands */
-    fun halt(bot: Int) { }
+    fun halt(bot: Int) {
+        for (listener in traceListeners) {
+            listener.onHalt(this, bot)
+        }
+    }
 
-    fun wait(bot: Int) { }
+    fun wait(bot: Int) {
+        for (listener in traceListeners) {
+            listener.onWait(this, bot)
+        }
+    }
 
-    fun flip(bot: Int) { }
+    fun flip(bot: Int) {
+        for (listener in traceListeners) {
+            listener.onFlip(this, bot)
+        }
+    }
 
-    fun sMove(bot: Int, dx: Int, dy: Int, dz: Int) { }
+    fun sMove(bot: Int, dx: Int, dy: Int, dz: Int) {
+        for (listener in traceListeners) {
+            listener.onSMove(this, bot, dx, dy, dz)
+        }
+    }
 
-    fun lMove(bot: Int, dx0: Int, dy0: Int, dz0: Int, dx1: Int, dy1: Int, dz1: Int) { }
+    fun lMove(bot: Int, dx0: Int, dy0: Int, dz0: Int, dx1: Int, dy1: Int, dz1: Int) {
+        for (listener in traceListeners) {
+            listener.onLMove(this, bot, dx0, dy0, dz0, dx1, dy1, dz1)
+        }
+    }
 
-    fun fission(bot: Int, dx: Int, dy: Int, dz: Int, m: Int) { }
+    fun fill(bot: Int, dx: Int, dy: Int, dz: Int) {
+        for (listener in traceListeners) {
+            listener.onFill(this, bot, dx, dy, dz)
+        }
+    }
 
-    fun fill(bot: Int, dx: Int, dy: Int, dz: Int) { }
+    fun fission(bot: Int, dx: Int, dy: Int, dz: Int, m: Int) {
+        for (listener in traceListeners) {
+            listener.onFission(this, bot, dx, dy, dz, m)
+        }
+    }
 
-    fun fusion(pBot: Int, sBot: Int) { }
-};
+    fun fusion(pBot: Int, sBot: Int) {
+        for (listener in traceListeners) {
+            // TODO: supply pBot/sBot coords
+            listener.onFusion(this, pBot, sBot, 0, 0, 0 ,0 ,0 ,0)
+        }
+    }
+}
---
+++ src/main/kotlin/TraceListener.kt
@@ -0,0 +1,13 @@
+package lambdallama.github.io
+
+interface TraceListener {
+    fun onHalt(state: State, bot: Int)
+    fun onWait(state: State, bot: Int)
+    fun onFlip(state: State, bot: Int)
+    fun onSMove(state: State, bot: Int, dx: Int, dy: Int, dz: Int)
+    fun onLMove(state: State, bot: Int, dx0: Int, dy0: Int, dz0: Int, dx1: Int, dy1: Int, dy2: Int)
+    fun onFill(state: State, bot: Int, dx: Int, dy: Int, dz: Int)
+    fun onFission(state: State, bot: Int, dx: Int, dy: Int, dz: Int, m: Int)
+    fun onFusion(state: State, pBot: Int, sBot: Int, pX: Int, pY: Int, pZ: Int,
+                 sX: Int, sY: Int, sZ: Int)
+}
