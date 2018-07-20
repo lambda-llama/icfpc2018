f5a8b3853a92583f6de4b9e4177bb6ee3e80ee9d
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-07-20 21:47:08 +0300

    simplify

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,29 @@
 plugins {
     application
     kotlin("jvm") version "1.2.51"
-}
-
-application {
-    mainClassName = "MainKt"
-}
-val gdxVersion = "1.9.4"
-dependencies {
-    compile(kotlin("stdlib-jre8"))
-    compile("khttp:khttp:0.1.0")
-    compile("com.google.guava:guava:25.1-jre")
-    compile(project(":jve"))
-    compile("com.badlogicgames.gdx:gdx-backend-lwjgl:$gdxVersion")
-    compile("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
-    testCompile("junit:junit:4.12")
+    java
 }
 
 repositories {
     jcenter()
+    mavenCentral()
+    maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
+    maven { setUrl("https://oss.sonatype.org/content/repositories/releases/") }
+
+}
+
+val gdxVersion = "1.9.4"
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("khttp:khttp:0.1.0")
+    compile("com.google.guava:guava:25.1-jre")
+    compile("com.badlogicgames.gdx:gdx:$gdxVersion")
+    compile("com.badlogicgames.gdx:gdx-backend-lwjgl:$gdxVersion")
+    compile("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
+
+    testCompile("junit:junit:4.12")
+}
+
+application {
+    mainClassName = "MainKt"
 }
--- jve/README.md
+++
@@ -1,4 +0,0 @@
-# java-voxel-engine
-Simple voxel rendering engine done in java/libgdx
-
-![Screenshot](/screenshot.png?raw=true)
