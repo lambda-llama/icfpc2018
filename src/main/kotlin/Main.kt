51a491335902ae793c61b9208fc9df24cdbbdcd8
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-07-20 22:00:56 +0300

    fix build.gradle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,6 @@ package io.github.lambdallama
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import io.github.lambdallama.vis.VoxelEngine
-import vis.VoxelEngine
 
 fun main(args: Array<String>) {
     val config = LwjglApplicationConfiguration().apply { forceExit = false }
