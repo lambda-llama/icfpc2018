94039a8baf00da88a2570e0285a3d3c9133de992
Sergei Lebedev <s.lebedev@criteo.com>
2018-07-20 23:46:25 +0200

    step it!

--- src/main/kotlin/Strategies.kt
+++ src/main/kotlin/Strategies.kt
@@ -3,14 +3,17 @@ package io.github.lambdallama
 import kotlin.math.min
 
 fun State.multiSMove(b: BotView, target: Coord) {
-    while (b.pos.x < target.x - 1) {
-        sMove(b.id, DeltaCoord(min(5, target.x - 1 - b.pos.x), 0, 0))
+    while (b.pos.x < target.x) {
+        sMove(b.id, DeltaCoord(min(5, target.x - b.pos.x), 0, 0))
+        step()
     }
-    while (b.pos.y < target.y - 1) {
-        sMove(b.id, DeltaCoord(0, min(5, target.y - 1 - b.pos.y), 0))
+    while (b.pos.y < target.y) {
+        sMove(b.id, DeltaCoord(0, min(5, target.y - b.pos.y), 0))
+        step()
     }
-    while (b.pos.z < target.z - 1) {
-        sMove(b.id, DeltaCoord(0, 0, min(5, target.z - 1 - b.pos.z)))
+    while (b.pos.z < target.z) {
+        sMove(b.id, DeltaCoord(0, 0, min(5, target.z - b.pos.z)))
+        step()
     }
 }
 
@@ -20,6 +23,7 @@ fun baseline(model: Model): State {
     val b = state.getBot(1)
 
     state.flip(b.id)
+    state.step()
     state.multiSMove(b, minCoord + DeltaCoord(-1, -1, -1))
 
     for (y in 0 until model.matrix.R) {
@@ -32,13 +36,16 @@ fun baseline(model: Model): State {
             val delta = coord - b.pos  // mlen == 1.
             if (model.matrix[coord]) {
                 state.fill(b.id, delta)
+                state.step()
             }
             state.sMove(b.id, delta)
+            state.step()
         }
     }
     check(state.matrix == model.matrix)
 
     state.flip(b.id)
+    state.step()
     state.multiSMove(b, Coord.ZERO)
     return state
 }
