436034fe9c11416f41b329031bbc738e8dea9c97
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-07-20 21:23:55 +0300

    simplify more

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     compile(kotlin("stdlib"))
     compile("khttp:khttp:0.1.0")
     compile("com.google.guava:guava:25.1-jre")
-    compile(project(":jve:core"))
+    compile(project(":jve"))
     compile("com.badlogicgames.gdx:gdx-backend-lwjgl:$gdxVersion")
     compile("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
     testCompile("junit:junit:4.12")
